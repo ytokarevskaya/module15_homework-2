90b8ee1ef7d92d4a0d5171c7fd63a1490dbbf102
ytokarevskaya <lan2214@yandex.ru>
2020-08-11 19:11:55 +0300

    Проверено

--- Task 1/Script.js
+++ Task 1/Script.js
@@ -1,12 +1,16 @@
-const btn1 = document.querySelector(".btn_1");
-const btn2 = document.querySelector(".btn_2");
+const btn = document.querySelector("button");
+const svg1 = document.querySelector(".btn_1");
+const svg2 = document.querySelector(".btn_2");
 
-btn1.addEventListener('click', () => {
-    btn1.style.display = "none";
-    btn2.style.display = "block";
+btn.addEventListener('click', () => {
+	if (svg1.style.display === "none") {
+		svg1.style.display = "";
+		svg2.style.display = "none";
+	} else {
+		svg1.style.display = "none";
+		svg2.style.display = "";
+	}
 });
 
-btn2.addEventListener('click', () => {
-    btn2.style.display = "none";
-    btn1.style.display = "block";
-});
+// Задание выполнено верно, но не оптимально. Создавать 2 разных кнопки, чтобы поменять иконку при нажатии - не лучшее решение :) 
+// Если у вас будет приложение с более сложной логикой, может возникнуть путаница с этими 2-мя кнопками, придётся дублировать обработчики событий и т.д. Лучше в одну кнопку положить 2 div'а с svg и показывать их поочередно (если спрятан первый, показывать второй и наоборот). Проверить это можно, посмотрев на свойство style.display у элемента. Исправила ваш код на более оптимальный.
--- Task 3/script.js
+++ Task 3/script.js
@@ -10,6 +10,9 @@ let websocket;
 btnOpen.addEventListener('click', () => {
     websocket = new WebSocket(wsUri);
 
+    websocket.onopen = function (evt) {
+        writeToScreen("CONNECTED");
+    };
     websocket.onclose = function (evt) {
         writeToScreen("DISCONNECTED");
     };
@@ -28,14 +31,31 @@ btnOpen.addEventListener('click', () => {
 //обьявляем функцию для вывода сообщений
 
 function writeToScreen(message) {
-    let pre = document.getElementById("vvod");
-    pre.innerHTML = message;
-    document.getElementById('output').innerHTML = message;
+    document.getElementById('output').innerHTML += `<div>${message}</div>`;
 }
 
 
 btnSend.addEventListener('click', () => {
-    const message = pre;
-    writeToScreen(message);
-    websocket.send(message);
+    if (websocket && websocket.readyState === 1) {
+        const message = document.querySelector("input").value;
+        writeToScreen(`MESSAGE: ${message}`);
+        websocket.send(message);
+    } else {
+        writeToScreen(
+            '<span style="color: red;">Соединение не установлено!</span>'
+        );
+    }
 });
+
+// В задании было несколько ошибок:
+
+// 1. В обработчике клика на btnSend использовалась переменная pre, которая была объявлена в другой функции, поэтому в обработчике эта переменная была недоступна. Из-за этого возникала ошибка и код не работал.
+
+// 2. Ошибка в функции writeToScreen:
+// let pre = document.getElementById("vvod");
+// pre.innerHTML = message;
+// Элемент с id vvod это инпут, у инпутов нет содержимого (innerHTML), поэтому записывать туда сообщение нет смысла. Инпуты предназначены для ввода информации, а для вывода в документе есть output с классом OknoVyvoda
+
+// 3. Это уже не ошибка, но важное замечание: после нажатия на кнопку "открыть соединение" не выводилось никакой информации о том, удалось установить соединение или нет. Если бы чатом пользовался обычный пользователь, он мог бы решить, что чат не работает, потому что после нажатия на кнопку ничего не происходит. Также было бы неплохо при попытке отправить сообщение писать предупреждение, что перед отправкой сначала нужно установить соединение.
+
+// В коде выше всё вышеперечисленное поправила
